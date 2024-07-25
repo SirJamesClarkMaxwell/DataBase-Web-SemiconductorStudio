4de3cd2b12fff8093a172f48de924529d2093e5f
Sir James Clarck Maxwell <fzabier@gmail.com>
2024-07-25 17:23:36 +0200

    Revert "still not compiling and linking"
    This reverts commit 624d5711a3e3d10042ab07826f5a798b93785a3e.

--- Web-DataBase-SemiconductorStudio/src/main.cpp
+++ Web-DataBase-SemiconductorStudio/src/main.cpp
@@ -1,7 +1,9 @@
 #include "pch.hpp"
 #include "tester.hpp"
 #include "main_json.hpp"
-#include "../cpr/include/cpr/cpr.h"
+//#include "../cpr/include/cpr/cpr.h"
+
+
 
 int main()
 {
@@ -9,20 +11,24 @@ int main()
 
 	// testingJSON();
 
+	 #if 0
+
 	Platform::Window::initProperties(1920, 1080, "Web-DataBase-SemiconductorStudio");
 	// TestedFitting to be initialized in the constructor of Tester
 	Tester t1{};
 	t1.run(UI::Components::draw_tester);
+	 
 
-#if 1
-	cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
-							   cpr::Authentication{"user", "pass", cpr::AuthMode::BASIC},
-							   cpr::Parameters{{"anon", "true"}, {"key", "value"}});
-	r.status_code;			  // 200
-	r.header["content-type"]; // application/json; charset=utf-8
-	r.text;					  // JSON text string
+
+	cpr::Response r = cpr::Get(cpr::Url{ "https://api.github.com/repos/whoshuu/cpr/contributors" },
+		cpr::Authentication{ "user", "pass", cpr::AuthMode::BASIC },
+		cpr::Parameters{ {"anon", "true"}, {"key", "value"} });
+	r.status_code;                  // 200
+	r.header["content-type"];       // application/json; charset=utf-8
+	r.text;                         // JSON text string
 
 	std::cout << r.text << std::endl;
+#endif
 	YAML::Node config = YAML::LoadFile("D:\\programming\\numerical\\SSweb\\bin\\Web-DataBase-SemiconductorStudio-Debug\\config.yaml");
 
 	/*if (config["lastLogin"]) {
@@ -32,10 +38,9 @@ int main()
 	const std::string username = config["username"].as<std::string>();
 	const std::string password = config["password"].as<std::string>();
 	std::cout << "Username: " << username << "\n";
-	std::cout << "Password: " << password << "\n";
+		std::cout << "Password: " << password << "\n";
 
 	std::ofstream fout("config.yaml");
 	fout << config;
-#endif
 	return 0;
 };
